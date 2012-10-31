26c40892c4cc2bae5e029dbe0abc3678fd8ee126
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-10-31 15:02:21 -0400

    Initial Commit

---
+++ RNSwipeViewController/RNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RNAppDelegate.h
+//  RNSwipeViewController
+//
+//  Created by Ryan Nystrom on 10/31/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RNSwipeViewController/RNViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNViewController.h
+//  RNSwipeViewController
+//
+//  Created by Ryan Nystrom on 10/31/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNViewController : UIViewController
+
+@end
